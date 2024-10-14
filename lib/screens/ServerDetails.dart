755693e2672a86c3f8d3c699b486735ea1f17443
Aadityajoshi151 <aadityajoshi151@gmail.com>
2024-10-14 20:07:21 +0530

    chore: adds form fields to server details page

--- lib/screens/ServerDetails.dart
+++ lib/screens/ServerDetails.dart
@@ -12,11 +12,40 @@ class _ServerdetailsState extends State<Serverdetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: const Center(child: Text('Server Details')),
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              TextFormField(
+                  decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Server URL',
+              )),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Token',
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  debugPrint('Add/Update pressed');
+                },
+                child: Text('Add/Update'),
+              )
+            ],
+          ),
+        ));
   }
 }

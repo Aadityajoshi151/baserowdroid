2dd98c1324bae41c9ef238bae5c4eeb62e4c55b8
Aadityajoshi151 <aadityajoshi151@gmail.com>
2024-10-22 09:57:50 +0530

    refactor: removes unecessary code

--- lib/widgets/TablesView.dart
+++ lib/widgets/TablesView.dart
@@ -31,30 +31,22 @@ class _TablesViewState extends State<TablesView> {
   }
 
   void _refreshUI() {
-    // This will be called after a delete operation to refresh the UI
-    _fetchTableData(); // Re-fetch the table data from the database
+    _fetchTableData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      //TODO: add ternary operator to display the message or the tables.
-      //Also use circularprogressindicator
-      //child: Text('No tables found'),
       child: FutureBuilder<List<TableData>>(
         future: dbHelper.getAllTableData(), // Fetch data from database
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            // Display a loading spinner while data is loading
             return Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-            // Handle any errors that might occur
             return Center(child: Text('Error: ${snapshot.error}'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            // Display a message if there is no data
             return Center(child: Text('No tables available'));
           } else {
-            // If data is available, display it in a ListView
             return ListView.builder(
               itemCount: tableDataList.length,
               itemBuilder: (context, index) {
